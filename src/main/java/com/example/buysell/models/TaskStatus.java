8b8b9f41f3f62bb098bbdbb849dcf8c86008164b
DobermanDD00 <doberman.dd00@gmail.com>
2024-01-04 15:18:02 +0300

    Стабильная без защиты

--- src/main/java/com/example/buysell/models/TaskStatus.java
+++ src/main/java/com/example/buysell/models/TaskStatus.java
@@ -15,7 +15,7 @@ public class TaskStatus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private long id;
     @Column(name = "title")
 
     private String title;
